009eae67f5a5d4c91b20e83a2130f3178557bf4d
github username <crishanks12@gmail.com>
2019-09-11 14:15:25 -0600

    users logout and logoutAll routes

--- src/routers/user.js
+++ src/routers/user.js
@@ -13,4 +13,37 @@ router.post('/users', async (req, res) => {
     }
 })
 
+router.post('/users/login', async (req, res) => {
+    try {
+        const user = User.findByCredentials(req.body.email, req.body.password)
+        const token = await user.generateAuthToken()
+        res.send({ user, token })
+    } catch(e) {
+        res.send(400).send()
+    }
+})
+
+router.post('/users/logout', auth, async (req, res) => {
+    try {
+        //  Get and remove the specific token that was used when user authenticated
+        req.user.tokens = req.user.tokens.filter((token) => {
+            return token.token !== req.token
+        })
+        await req.user.save()
+        res.send()
+    } catch(e) {
+        res.send(500).send()
+    }
+})
+
+router.post ('/users/logoutAll', auth, async (req, res) => {
+    try {
+        req.user.tokens = []
+        await req.user.save()
+        res.send()
+    } catch(e) {
+        req.status(500).send()
+    }
+})
+
 module.exports = router
